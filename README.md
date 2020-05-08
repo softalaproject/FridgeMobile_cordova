a9dabef23f7baa218690fe915294075ee6cc0df2
nikosul <niko.sulkava@myy.haaga-helia.fi>
2020-05-08 13:52:31 +0300

    IP address up to date

--- README.md
+++ README.md
@@ -2,4 +2,17 @@
 
 A PhoneGap App
 
+## Requirements
+
+npm install -g phonegap <br>
+npm install node_modules <br>
+
+to locally test the app <br>
+PhoneGap Developer app <br>
+https://play.google.com/store/apps/details?id=com.adobe.phonegap.app&hl=en_GB
+
 ## Usage
+
+phonegap serve <br>
+[phonegap] starting app server... <br>
+[phonegap] listening on xxx.xxx.x.x:xxxx <br>
--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -17,23 +17,15 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+    // IP address with port
     onDeviceReady: function() {
-        cordova.InAppBrowser.open('http://95.217.134.247:8100/', '_self', 'location=no');
+        cordova.InAppBrowser.open('http://95.217.134.247/', '_self', 'location=no');
         window.open = cordova.InAppBrowser.open;
     },
     // Update DOM on a Received Event
@@ -44,7 +36,6 @@ var app = {
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-
         console.log('Received Event: ' + id);
     }
 };
